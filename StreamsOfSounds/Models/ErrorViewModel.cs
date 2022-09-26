72c9b45e554f732e6d28551da2079928be0da007
irj101 <jirena629@gmail.com>
2022-09-26 17:10:03 -0400

    Add project files.

---
+++ StreamsOfSounds/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StreamsOfSounds.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
