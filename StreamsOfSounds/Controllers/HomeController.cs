410bd4a2e1618f15d3061f3609fa72067ff5ae3c
mariarea01 <66438113+mariarea01@users.noreply.github.com>
2022-12-09 15:51:08 -0500

    No need for Privacy index and in home controller

--- StreamsOfSounds/Controllers/HomeController.cs
+++ StreamsOfSounds/Controllers/HomeController.cs
@@ -18,11 +18,6 @@ namespace StreamsOfSounds.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         public IActionResult CreateOpportunity()
         {
             return View();
