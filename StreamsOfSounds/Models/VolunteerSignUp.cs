8a1bdd31d54d57cf8ee7eef88a492d4729797c76
mariarea01 <66438113+mariarea01@users.noreply.github.com>
2022-12-09 15:48:28 -0500

    VolunteerSignUp Project

---
+++ StreamsOfSounds/Models/VolunteerSignUp.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace StreamsOfSounds.Models
+{
+    public class VolunteerSignUp
+    {
+        
+    [Display(Name = "Serial No")]
+        public byte EmployeeId { get; set; }
+
+        [Display(Name = "Name")]
+        public string EmployeeName { get; set; }
+        public string Address { get; set; }
+    }
+}
