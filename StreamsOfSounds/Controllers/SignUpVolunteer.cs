bec3aa2feafc19f9f78a5965b475c85d5744d702
mariarea01 <66438113+mariarea01@users.noreply.github.com>
2022-12-09 16:04:31 -0500

    Added in My Opportunity List

---
+++ StreamsOfSounds/Controllers/SignUpVolunteer.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StreamsOfSounds.Models;
+using System.Diagnostics;
+
+namespace StreamsOfSounds.Controllers
+{
+    public class SignUpVolunteer : Controller
+    {
+        public IActionResult MyOpportunityList()
+        {
+            return View();
+        }
+    }
+}
