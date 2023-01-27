9d94793fac9932632ab7f2219bb56402b4d7b799
mariarea01 <66438113+mariarea01@users.noreply.github.com>
2023-01-27 09:20:18 -0500

    VolSignUpController merge

--- StreamsOfSounds/Controllers/VolunteerSignUpController.cs
+++ StreamsOfSounds/Controllers/VolunteerSignUpController.cs
@@ -7,20 +7,12 @@ namespace StreamsOfSounds.Controllers
     public class VolunteerSignUpController : Controller
     {
         [HttpPost]
-        public ActionResult form1(string txtName, string txtEmail, string txtInstrument)
-        {
-            ViewBag.Name = txtName;
-            ViewBag.Email = txtEmail;
-            ViewBag.Instrument = txtInstrument;
-
-            return View("Date");
-        }
 
         [HttpPost]
         public ActionResult SignUp(VolunteerSignUpFormRequest request)
         {
             // TODO.MR: Create the object for the database, add to database, return good view, and validation
-            ViewBag.FirstName = request.FirstName; 
+            ViewBag.FirstName = request.FirstName;
             ViewBag.LastName = request.LastName;
             ViewBag.Email = request.Email;
             ViewBag.Phone = request.PhoneNumber;
